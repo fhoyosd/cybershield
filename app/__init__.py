6a220a6aa8367f9d9d0603a3164253b076a4fe0f
fhoyosd <fhoyosd@ucentral.edu.co>
2025-10-28 10:02:32 -0500

    primer commit

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .config import Config
+
+from .routes.auth import auth_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    app.register_blueprint(auth_bp)
+
+    return app
